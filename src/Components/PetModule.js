48f98dddb1a94822509313d4ccd19677d4c5d11b
Evan Garrett <evang522@gmail.com>
2018-04-13 14:46:48 -0400

    Add store logic to variable

--- src/Components/PetModule.js
+++ src/Components/PetModule.js
@@ -2,24 +2,48 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 
-export default class PetModule extends React.Component{
-  
+
+export class PetModule extends React.Component{
+
+
+  componentDidMount() {
+    // include some logic for checking the prop passed to the module
+    // to see if this is for displaying cat or dog
+    if (this.props.animalType==='cat') {
+      // run catdisplay api call
+    } else if (this.props.animalType==='dog') {
+      //run dogdisplay api call
+    }
+  }
+
+  onAdopt() {
+    // include some logic for checking the prop passed to the module
+    // to see if this is display cat or dog
+    if (this.props.animalType==='cat') {
+      // run catdelete api call
+    } else if (this.props.animalType==='dog') {
+      //run dogdelete api call
+    }
+  }
+
+
   render() {
 
+  
     return(
       <section className='pet-module'>
         <div className='pet-name'>
-        Bernie
+        {this.props.cat.name}
         </div>
         <section className='pet-info-pic-cont'>
           <section className='pet-pic'>
-            <img src='https://kcpetproject.org/wp-content/uploads/cat-hammock-332x250.jpg' alt='dog' />
+            <img src={this.props.cat.imageURL} alt={this.props.cat.imageDescription} />
           </section>
           <section className='pet-info'>
             <section className='pet-info-about'>
-            <h4 className='pet-info-about-intro'>About Bernie:</h4>
+            <h4 className='pet-info-about-intro'>About {this.props.cat.name}:</h4>
             <p className='pet-info-about-text'>
-              HI!
+             {this.props.cat.story}
             </p>
             </section>
           </section>
@@ -28,28 +52,27 @@ export default class PetModule extends React.Component{
           <section className='more-info'>
             <section className='sex additional-info-box'>
               <div className='info-title'>Sex:</div>
-              Male
+              {this.props.cat.sex}
             </section>
             <section className='age additional-info-box'>
             <div className='info-title'>Age:</div>
-              3
+            {this.props.cat.age}
             </section>
             <section className='breed additional-info-box'>
             <div className='info-title'>Breed:</div>
-            Tabby
+            {this.props.cat.breed}
             </section>
           </section>
-          <button className='adopt-button'>Adopt</button>
-
+          <button onClick={() => this.onAdopt()} className='adopt-button'>Adopt</button>
       </section>
     )
   }
 }
 
 
-// const mapStateToProps = state => ({
-//   cat: state.reducers.cat[0],
-//   dog: state.reducers.dog[0]
-// })
+const mapStateToProps = state => ({
+  cat: state.cat,
+  dog: state.dog
+})
 
-// export default connect(mapStateToProps(PetModule));
+export default connect(mapStateToProps(PetModule));
